10817d9cdc0bc7f73859a5d969839856423a3a77
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-07-10 15:00:32 +0200

    array and filter

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-
+  // @Output() onEvent: EventEmitter<string> = new EventEmitter<string>();
+  musicArray: string[] = [];
   constructor(private formBuilder: FormBuilder) {}
 
   formPlayer = this.formBuilder.group({
-    add : [ 'file', Validators.required],
-    // select : ['']
-  })
+    add: ['file', Validators.required],
+    select : ['']
+  });
+
+  addMusic(music: any) {
+    this.musicArray.push(music);
+  }
+
+  deleteMusic(event: string) {
+this.musicArray=this.musicArray.filter(item => event !== item);
+  console.log(this.musicArray);
+  
+  }
 }
--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.css']
+  styleUrls: ['./playlist.component.css'],
 })
 export class PlaylistComponent {
-
+  @Input() music!: string;
+  @Output() deleteMusic: EventEmitter<string> = new EventEmitter();
+  onDelete() {
+    this.deleteMusic.emit(this.music);
+  }
 }
