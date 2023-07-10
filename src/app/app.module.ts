33c304811800b7a5ee6b54a2eb3d2bd97dce0d3c
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-07-10 11:36:01 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { PlayerComponent } from './player/player.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    PlayerComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
